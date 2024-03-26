9dd48a0aa7322e43935342f8ea956bca010dbf3b
Huzaifa Mukhtar <159668700+Huzaifa4412@users.noreply.github.com>
2024-03-26 15:58:28 +0500

    Update Game

--- Gussing A Number/index.ts
+++ Gussing A Number/index.ts
@@ -1,14 +1,39 @@
 // Guessing a "Number",
 
 import inquirer from "inquirer";
-let compgues = 3;
-let userguess = await inquirer.prompt([
-    {
-        message:"Enter a number between 1 to 10",
-        type:"number",
-        name: "userguess",
+let compgues = Math.floor(Math.random()*10+1);
+let answer = await inquirer.prompt([
+  {
+    message: "Enter a number between 1 to 10",
+    type: "number",
+    name: "userguess",
+  },
+]);
+if (answer.userguess >=1 && answer.userguess <= 10) {
+  console.log(answer.userguess);
+  if (answer.userguess === compgues) {
+    console.log("You Won");
+    console.log(`Computer number is:  ${compgues} and your number is:  ${answer.userguess}` ) ;
+}
+else{
+    if(answer.userguess < compgues-1){
+        console.log("Number is less than computer number");
+    console.log(`Computer number is:  ${compgues} and your number is:  ${answer.userguess}` ) ;
+
     }
-])
-if(us){
-console.log(userguess.userguess);
+    else if(answer.userguess > compgues+1){
+        console.log("Number is greater than computer number");
+    console.log(`Computer number is:  ${compgues} and your number is:  ${answer.userguess}` ) ;
+
+    }
+    else{
+        console.log("Number is Very close to computer number");
+    console.log(`Computer number is:  ${compgues} and your number is:  ${answer.userguess}` ) ;
+
+}
+}
+}
+else{
+    console.log("Please Enter the Number 1-10");
+    
 }
