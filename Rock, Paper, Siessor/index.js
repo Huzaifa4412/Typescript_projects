33d4d76e0c149bc9f1dfb7d6b2e6f1c053075b1b
Huzaifa Mukhtar <159668700+Huzaifa4412@users.noreply.github.com>
2024-03-27 02:02:04 +0500

    update file

---
+++ Rock, Paper, Siessor/index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+console.log("Welcome to Rock, Paper, Siessor Game");
+console.log("For Siessor type  '1");
+console.log("For Paper type  '2");
+console.log("For Rock type  '3");
+let compopt = Math.floor(Math.random() * 3 + 1);
+let answer = await inquirer.prompt([
+    {
+        message: "Please Select your option",
+        name: "useropt",
+        type: "number",
+    }
+]);
+if (answer.useropt >= 1 && answer.useropt <= 3) {
+    if (answer.useropt === 1) {
+        console.log("You Select Siessor");
+    }
+    else if (answer.useropt === 2) {
+        console.log("You Select Paper");
+    }
+    else {
+        console.log("You Select Rock");
+    }
+    let compopt = Math.floor(Math.random() * 3 + 1);
+    if (compopt === 1) {
+        console.log("Computer Select Siessor");
+    }
+    else if (compopt === 2) {
+        console.log("Computer Select Paper");
+    }
+    else {
+        console.log("Computer Select Rock");
+    }
+    if (answer.useropt === compopt) {
+    }
+}
+else {
+    console.log("Please Enter the Number between 1-3");
+}
+console.log(`Thank you for using this program ❤  😍`);
+console.log(`Developed By: Huzaifa Mukhtar`);
