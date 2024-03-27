5a6b1ea2b6c72201c5dc41e101bcd616283fd001
Huzaifa Mukhtar <159668700+Huzaifa4412@users.noreply.github.com>
2024-03-28 04:00:40 +0500

    Update index.js

--- Email, password, cheaker/index.js
+++ Email, password, cheaker/index.js
@@ -1,35 +1,38 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 const recieve = await inquirer.prompt([
     {
-        message: 'Please Enter Your username',
-        name: 'username',
-        type: 'input'
+        message: "Please Enter Your username",
+        name: "username",
+        type: "input",
     },
     {
-        message: 'Please Enter Your Password',
-        name: 'password',
-        type: 'password'
-    }
+        message: "Please Enter Your Password",
+        name: "password",
+        type: "password",
+    },
 ]);
 console.log(`Please Enter Your username and Password again`);
 let usernameConfirm = await inquirer.prompt({
-    message: 'Please Enter Your username Again to confirm',
-    name: 'username-com',
+    message: "Please Enter Your username Again to confirm",
+    name: "username-com",
 });
 let passwordComfirm = await inquirer.prompt({
-    message: 'Please Enter Your Password Again to confirm',
-    name: 'password-com'
+    message: "Please Enter Your Password Again to confirm",
+    name: "password-com",
 });
-if (recieve.username == usernameConfirm['username-com'] && recieve.password == passwordComfirm['password-com']) {
-    console.log('You are login');
+if (recieve.username == usernameConfirm["username-com"] &&
+    recieve.password == passwordComfirm["password-com"]) {
+    console.log("You are login");
     console.log(`You Enter The Correct Password`);
 }
-else if (recieve.username != usernameConfirm['username-com']) {
+else if (recieve.username != usernameConfirm["username-com"]) {
     console.log(`Please Enter the correct username`);
 }
-else if (recieve.password != passwordComfirm['password-com']) {
+else if (recieve.password != passwordComfirm["password-com"]) {
     console.log(`Please Enter the correct Password`);
 }
 else {
     console.log(`Please Enter the correct username and Password`);
 }
+console.log(`Thank you for using our program`);
+console.log(`Developed By Huzaifa Mukhtar`);
