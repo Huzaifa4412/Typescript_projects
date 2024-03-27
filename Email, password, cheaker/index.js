590ca9bd2ab6eedf11d88bff0c6b54619d5863e2
Huzaifa Mukhtar <159668700+Huzaifa4412@users.noreply.github.com>
2024-03-28 03:39:55 +0500

    update

--- Email, password, cheaker/index.js
+++ Email, password, cheaker/index.js
@@ -1,1 +1,14 @@
-export {};
+import inquirer from 'inquirer';
+const recieve = await inquirer.prompt([
+    {
+        message: 'Please Enter Your Email',
+        name: 'email',
+        type: 'email'
+    },
+    {
+        message: 'Please Enter Your Password',
+        name: 'password',
+        type: 'password'
+    }
+]);
+console.log(recieve);
--- Email, password, cheaker/index.ts
+++ Email, password, cheaker/index.ts
@@ -1,1 +1,15 @@
-// Email , 
+import inquirer from 'inquirer'
+const recieve = await inquirer.prompt([
+    {
+        message:'Please Enter Your Email',
+        name:'email',
+        type:'email'
+    },
+    {
+        message:'Please Enter Your Password',
+        name:'password',
+        type:'password'
+    }
+])
+console.log(`Please Enter Your Email and Password again `);
+
